ec303a5f0c20323105f8a5ddaf00dee8a356c4fc
Brian Favis <59325056+bfavis2@users.noreply.github.com>
2025-08-25 13:20:33 -0500

    hyperland config changes

--- scripts/install-scripts/install-packages.sh
+++ scripts/install-scripts/install-packages.sh
@@ -2,4 +2,7 @@
 
 # Install AUR packages using yay
 yay -S --noconfirm --needed \
-  stow
+  stow \
+  pipewire wireplumber pipewire-pulse \
+  sof-firmware pipewire-audio pipewire-alsa \
+  alsa-mixer pipewire-docs
